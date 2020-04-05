b0e66e4d90f424b3508db7ad5784218d686cc9cd
Burton Chen <burton.c33@gmail.com>
2020-04-05 18:19:17 +1000

    Revert "Revert "Backend/menu""

--- project/waiter/customer/serializers.py
+++ project/waiter/customer/serializers.py
@@ -1,20 +1,36 @@
 from django.contrib.auth.models import User
-from .models import MenuItem, MenuCategory
+from .models import MenuItem, MenuCategory, OrderList, OrderRequest
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    
     class Meta:
         model = User
         fields = ['username', 'email']
 
-class MenuItemSerializer(serializers.HyperlinkedModelSerializer): 
-    class Meta:
-        model = MenuItem
-        fields = ['name', 'price']
-
-class MenuCategorySerializer(serializers.HyperlinkedModelSerializer):
-    menuItem = serializers.ReadOnlyField(source='menuItem.name')
+class MenuCategorySerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = MenuCategory
-        fields = ['name', 'menuItem']
+        fields = ['id', 'name', 'menu_item']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'name', 'price', 'description', 'menu_category']
+
+class OrderListSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = OrderList
+        fields = ['id', 'owner', 'order_request']
+
+class OrderRequestSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = OrderRequest
+        fields = ['id', 'order_list', 'owner', 'menu_item', 'comments', 'quantity']
