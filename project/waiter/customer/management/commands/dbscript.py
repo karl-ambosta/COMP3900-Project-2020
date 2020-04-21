2d31aa262d41ceb45de67d9170f68ba7033c8ca4
Silvia Xu <52628007+silviaseal@users.noreply.github.com>
2020-04-21 18:46:10 +1000

    added opening hours

--- project/waiter/customer/management/commands/dbscript.py
+++ project/waiter/customer/management/commands/dbscript.py
@@ -91,15 +91,13 @@ class Command(BaseCommand):
         """
         Opening Hours
         """
-    
-        #for i in range (1,8):
         restaurant1 = Restaurant.objects.get(name='UNSW Cafe')
-
-        restaurant1_hours = OpeningHours.objects.create(restaurant = restaurant1, day = 1, from_hour = datetime.time(16, 00),to_hour = datetime.time(17, 00))
-        restaurant1_hours.save()
-
-
-        
-        
-
-    
+        restaurant2 = Restaurant.objects.get(name='Anzac Parade Cafe')
+        restaurant3 = Restaurant.objects.get(name = 'Barker St Cafe')
+        for i in range(1,8): 
+            restaurant1_hours = OpeningHours.objects.create(restaurant = restaurant1, day = i, from_hour = datetime.time(6, 00),to_hour = datetime.time(17, 00))
+            restaurant2_hours = OpeningHours.objects.create(restaurant = restaurant2, day = i, from_hour = datetime.time(6, 00),to_hour = datetime.time(17, 00))
+            restaurant3_hours = OpeningHours.objects.create(restaurant = restaurant3, day = i, from_hour = datetime.time(6, 00),to_hour = datetime.time(17, 00))
+            restaurant1_hours.save()
+            restaurant2_hours.save()
+            restaurant3_hours.save()
